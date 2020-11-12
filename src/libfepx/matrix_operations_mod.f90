f8fe804bbf45bb3a563fef2deac3f81881e3ab13
mkasemer <mkasemer@eng.ua.edu>
2020-11-12 17:53:35 -0600

    merged parallel_matrix_mod into matrix_operations_mod

--- src/libfepx/matrix_operations_mod.f90
+++ src/libfepx/matrix_operations_mod.f90
@@ -11,6 +11,8 @@ MODULE MATRIX_OPERATIONS_MOD
 ! CALC_ELVOL: Calculates element volumes.
 ! DETERMINANT_GRN: Calculates determinants of an array of 3x3 matrices
 ! FIND_INDICES: Find the members of vector equal to target, return val and ind.
+! GEN_MATRIX_VECTOR_MULT: Matrix times vector
+! GEN_MATRIX_MULT: Matrix times matrix
 ! INVERT5X5: Inverts an array of 5x5 matrices.
 ! LATTICE_DEFORM: Change coordinates from sample to lattice (for 5-vectors).
 ! LATTICE_DEFORM_SER: Serial version (one element).
@@ -34,6 +36,7 @@ MODULE MATRIX_OPERATIONS_MOD
 ! ROT_MAT_SKEW_SER: Serial version (one element).
 ! ROT_MAT_SYMM: Construct 5x5 rotation matrix acting on 5-vectors.
 ! ROT_MAT_SYMM_SER: Serial version (one element).
+! SPARSE_MATVEC_EBE: Matrix times vector, element by element
 ! SYMM_VGR: Compute symmetric part of an array of velocity gradients.
 ! SYMM_VGR_SER: Serial version (one element).
 ! SKEW_VGR: Compute skew part of an array of velocity gradients.
@@ -55,6 +58,10 @@ USE DIMENSIONS_MOD
 USE QUADRATURE_MOD
 USE READ_INPUT_MOD
 USE SHAPE_3D_MOD
+!
+! From libparallel:
+!
+USE GATHER_SCATTER_MOD
 !
 IMPLICIT NONE
 !
@@ -234,6 +241,139 @@ CONTAINS
     RETURN
     !
     END SUBROUTINE FIND_INDICES
+    !
+    !===========================================================================
+    !
+    SUBROUTINE GEN_MATRIX_VECTOR_MULT(Y, A, X, I1, I2, I3, I4, IER)
+    !
+    !  Array of matrices times array of vectors: y(i) = A(i)*x(i)
+    !
+    ! Note that the arrays x,y,a are all assumed shape arrays and that they are
+    !   therefore dimensioned 1:p, 1:q, etc., even though they may be
+    !   dimensioned differently in the calling routine.
+    !
+    !---------------------------------------------------------------------------
+    !
+    ! Arguments:
+    ! Y:
+    ! A:
+    ! X:
+    ! I1:
+    ! I2:
+    ! I3:
+    ! I4:
+    ! IER:
+    !
+    REAL(RK) :: Y(:,:)
+    REAL(RK) :: A(:,:,:)
+    REAL(RK) :: X(:,:)
+    INTEGER :: I1
+    INTEGER :: I2
+    INTEGER :: I3
+    INTEGER :: I4
+    INTEGER :: IER
+    !
+    ! Locals:
+    !
+    INTEGER :: I
+    INTEGER :: J
+    INTEGER :: K
+    INTEGER :: AD1
+    INTEGER :: AD2
+    INTEGER :: AD3
+    !
+    !---------------------------------------------------------------------------
+    !
+    AD1 = UBOUND(A, 1)
+    AD2 = UBOUND(A, 2)
+    AD3 = UBOUND(A, 3)
+    !
+    DO K = 1, AD3
+        !
+        DO J = 1, AD2
+            !
+            !DO I = 1, AD1
+            Y(:, K) = Y(:, K) + A(:, J, K) * X(J, K)
+            !END DO
+            !
+        END DO
+        !
+    END DO
+    !
+    IER = 0
+    !
+    RETURN
+    !
+    END SUBROUTINE GEN_MATRIX_VECTOR_MULT
+    !
+    !===========================================================================
+    !
+    SUBROUTINE GEN_MATRIX_MULT(C, A, B, I1, I2, IER)
+    !
+    ! Accumulate Array of matrices times array of matrices: c(i) = a(i) * b(i)
+    !
+    ! Note: c is not zeroed in this subroutine, so the values are accumulated;
+    !   in all the calling routines, however, the c array is zeroed before
+    !   calling this routine.
+    !
+    !---------------------------------------------------------------------------
+    !
+    ! Arguments:
+    ! C: Array of result matrices (c=a*b)
+    ! A, B: Array of input matrices
+    ! I1, I2: Not used (MPK: So why are they here?)
+    ! IER: Return status (0 = okay)
+    !
+    REAL(RK) :: C(:,:,:)
+    REAL(RK) :: A(:,:,:)
+    REAL(RK) :: B(:,:,:)
+    INTEGER :: I1
+    INTEGER :: I2
+    INTEGER :: IER
+    !
+    ! Locals:
+    !
+    INTEGER :: I
+    INTEGER :: J
+    INTEGER :: K
+    INTEGER :: M
+    INTEGER :: LDA
+    INTEGER :: LDB
+    INTEGER :: LDC
+    INTEGER :: LTA
+    INTEGER :: LTB
+    INTEGER :: N
+    !
+    !---------------------------------------------------------------------------
+    !
+    LDA = UBOUND(A, 1)
+    LDB = UBOUND(B, 1)
+    LDC = UBOUND(C, 1)
+    LTA = UBOUND(A, 2)
+    LTB = UBOUND(B, 2)
+    N = UBOUND(A, 3)
+    !
+    DO M = 1, N
+        !
+        DO J = 1, LTB
+            !
+            DO K = 1, LTA
+                !
+!               DO I = 1,LDC
+                C(:, J, M) = C(:, J, M) + A(:, K, M) * B(K, J, M)
+!               END DO
+                !
+            END DO
+            !
+        END DO
+        !
+    END DO
+    !
+    IER = 0
+    !
+    RETURN
+    !
+    END SUBROUTINE GEN_MATRIX_MULT
     !
     !===========================================================================
     !
@@ -1552,6 +1692,74 @@ CONTAINS
     RETURN
     !
     END SUBROUTINE ROT_MAT_SYMM_SER
+    !
+    !===========================================================================
+    !
+    SUBROUTINE SPARSE_MATVEC_EBE(RES, SOL, TEMP1, TEMP2, GSTIF, BCS, NNPE, &
+        & NSUB, NSUP, ESUB, ESUP, DTRACE, NP)
+    !
+    !  Matrix vector multiply, element by element.
+    !
+    !---------------------------------------------------------------------------
+    !
+    ! Arguments:
+    ! RES:
+    ! SOL:
+    ! TEMP1:
+    ! TEMP2:
+    ! GSTIF:
+    ! BCS:
+    ! NNPE:
+    ! NSUB:
+    ! NSUP:
+    ! ESUB:
+    ! ESUP:
+    ! DTRACE:
+    ! NP:
+    !
+    REAL(RK) :: RES(NSUB:NSUP)
+    REAL(RK) :: SOL(NSUB:NSUP)
+    REAL(RK) :: TEMP1(0:(NNPE - 1), ESUB:ESUP)
+    REAL(RK) :: TEMP2(0:(NNPE - 1), ESUB:ESUP)
+    REAL(RK) :: GSTIF(0:(NNPE - 1), 0:(NNPE - 1), ESUB:ESUP)
+    LOGICAL :: BCS(NSUB:NSUP)
+    INTEGER :: NNPE
+    INTEGER :: NSUB
+    INTEGER :: NSUP
+    INTEGER :: ESUB
+    INTEGER :: ESUP
+    TYPE(TRACE) :: DTRACE
+    INTEGER :: NP(0:(NNPE-1),ESUB:ESUP)
+    !
+    !  Locals:
+    !
+    INTEGER :: IER
+    INTEGER :: I
+    INTEGER :: J
+    INTEGER :: IDUMMY
+    !
+    !---------------------------------------------------------------------------
+    !
+    CALL PART_GATHER(TEMP1, SOL, NP, DTRACE)
+    !
+    TEMP2 = 0.0D0
+    !
+    CALL GEN_MATRIX_VECTOR_MULT(TEMP2, GSTIF, TEMP1, IDUMMY, IDUMMY, IDUMMY, &
+        & IDUMMY, IER)
+    !
+    RES = 0.0D0
+    !
+    CALL PART_SCATTER(RES, TEMP2, NP, .FALSE., DTRACE)
+    !
+    WHERE (BCS)
+        !
+        RES = 0.0D0
+        !
+    END WHERE
+    !
+    RETURN
+    !
+    END SUBROUTINE SPARSE_MATVEC_EBE
     !
     !===========================================================================
     !
